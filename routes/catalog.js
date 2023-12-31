dd6c3d61c8430ef83f4a83cdab854134f7f9da8b
JakeBrowning90 <jacobbrowning90@gmail.com>
2023-12-31 10:32:44 -0500

    Add all placeholder routes for artists and pieces pages

--- routes/catalog.js
+++ routes/catalog.js
@@ -2,58 +2,60 @@ const express = require("express");
 const router = express.Router();
 
 // Require controller modules
-const artist_controller = require("../controllers/artistController");
 const piece_controller = require("../controllers/pieceController");
-
-// ARTIST ROUTES
-// GET request for creating Artist
-router.get();
-
-// POST request for creating Artist
-router.post();
-
-// GET request to delete Artist
-router.get();
-
-// POST request to delete Artist
-router.post();
-
-// GET request to update Artist
-router.get();
- 
-// POST request to update Artist
-router.post();
-
-// GET request for one Artist
-router.get();
-
-// GET request for all Artist
-router.get();
+const artist_controller = require("../controllers/artistController");
 
 // PIECE ROUTES
 // GET catalog home page
 router.get("/", piece_controller.index);
 
 // GET request for creating Piece
-router.get();
+router.get("/piece/create", piece_controller.piece_create_get);
 
 // POST request for creating Piece
-router.post();
+router.post("/piece/create", piece_controller.piece_create_post);
 
 // GET request to delete Piece
-router.get();
+router.get("/piece/:id/delete", piece_controller.piece_delete_get);
 
 // POST request to delete Piece
-router.post();
+router.post("/piece/:id/delete", piece_controller.piece_delete_post);
 
 // GET request to update Piece
-router.get();
+router.get("/piece/:id/update", piece_controller.piece_update_get);
  
 // POST request to update Piece
-router.post();
+router.post("/piece/:id/update", piece_controller.piece_update_post);
 
 // GET request for one Piece
-router.get();
+router.get("/piece/:id", piece_controller.piece_detail);
 
-// GET request for all Piece
-router.get();
+// GET request for all Pieces
+router.get("/pieces", piece_controller.piece_list);
+
+// ARTIST ROUTES
+// GET request for creating Artist
+router.get("/artist/create", artist_controller.artist_create_get);
+
+// POST request for creating Artist
+router.post("/artist/create", artist_controller.artist_create_post);
+
+// GET request to delete Artist
+router.get("/artist/:id/delete", artist_controller.artist_delete_get);
+
+// POST request to delete Artist
+router.post("/artist/:id/delete", artist_controller.artist_delete_post);
+
+// GET request to update Artist
+router.get("/artist/:id/update", artist_controller.artist_update_get);
+ 
+// POST request to update Artist
+router.post("/artist/:id/update", artist_controller.artist_update_post);
+
+// GET request for one Artist
+router.get("/artist/:id", artist_controller.artist_detail);
+
+// GET request for all Artists
+router.get("/artists", artist_controller.artist_list);
+
+module.exports = router;
