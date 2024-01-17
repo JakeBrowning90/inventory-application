8b2ba5809d1254fcfe86bba4e4529455f9342779
JakeBrowning90 <jacobbrowning90@gmail.com>
2024-01-17 15:41:42 -0500

    Add image upload

--- app.js
+++ app.js
@@ -49,12 +49,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/catalog', catalogRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
